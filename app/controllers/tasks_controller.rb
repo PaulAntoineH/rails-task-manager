a67ab2f207187b6d2a5f4979b9a7c1ea79dd4780
PaulAntoine Huilizen <paul.antoine.huilizen@gmail.com>
2018-11-20 16:06:42 +0100

    second exercise

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,47 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy, :mark_as_done]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_params)
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  def mark_as_done
+    @task.completed = !@task.completed
+    @task.save
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # read all tasks
+  get 'tasks', to: 'tasks#index'
+  # create one task
+  get 'tasks/new', to: 'tasks#new', as: 'new'
+  post 'tasks', to: 'tasks#create'
+  # read one task
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  # edit one task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+  patch 'tasks/:id', to: 'tasks#update', as: 'update'
+  # delete one task
+  delete 'tasks/:id', to: 'tasks#destroy', as: "delete"
+  # mark task as done
+  get 'tasks/:id/mark_as_done', to: 'tasks#mark_as_done', as: "done"
 end
